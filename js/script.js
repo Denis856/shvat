92e54d7da088b071e8044761c1c6277bbb0da955
Den89 <malyhindenis8@gmail.com>
2023-04-23 18:55:58 +0300

    uu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+var burger = document.querySelector('.header__burger');
+var block0 = document.querySelectorAll('.footer__block');
+
+burger.addEventListener('click', ()=> {
+   burger.classList.toggle('active');
+   document.querySelector('.header__list').classList.toggle('active');
+   document.querySelector('body').classList.toggle('active');
+   block0.forEach(item =>{
+      item.classList.remove('active');
+   });
+});
+
+
+// next
+
 var title01 = document.querySelectorAll('.footer__title');
 
 title01.forEach(item => {
